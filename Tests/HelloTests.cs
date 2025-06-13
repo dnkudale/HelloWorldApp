84e8c0ecd35cb3fcae1638d1abd7554ffe91c516
dnkudale <dnkudale97@gmail.com>
2025-06-13 12:45:31 +0530

    Create HelloTests.cs

---
+++ Tests/HelloTests.cs
@@ -0,0 +1,16 @@
+using Xunit;
+using HelloWorldApp;
+ 
+namespace HelloWorldApp.Tests
+{
+    public class HelloTests
+    {
+        [Fact]
+        public void GetMessage_ReturnsCorrectMessage()
+        {
+            string expected = "Hello, World from .NET!";
+            string actual = Program.GetMessage();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
