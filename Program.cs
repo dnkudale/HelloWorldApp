299b7a4367b65ce3676f2fd8b0ba6132ade6d029
dnkudale <dnkudale97@gmail.com>
2025-06-13 12:36:29 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-
+using System;
+ 
+namespace HelloWorldApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(GetMessage());
+        }
+ 
+        public static string GetMessage()
+        {
+            return "Hello, World from .NET!";
+        }
+    }
+}
